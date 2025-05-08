28abc50d0e12b62bac27e3756f6a3c082c452ad9
Gábor <deutschgabor92@gmail.com>
2025-05-08 20:11:54 +0200

    undo changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const nodemailer = require("nodemailer");
-const dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
